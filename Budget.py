0190b1d849437adba2bd68ccb2c4766ab04b8ede
SharondaB88 <81206468+sharondab88@users.noreply.github.com>
2021-04-26 02:55:54 -0400

    commit

---
+++ Budget.py
@@ -0,0 +1,63 @@
+#global amount
+#global balance
+#helps keep running total(s)
+
+import time
+localtime = time.asctime(time.localtime(time.time()))
+print("Current local time is :", localtime)
+
+class Budget:
+
+    def __init__(self, category, amount):
+        self.category = category
+        self.amount = amount
+
+    # methods
+    def deposit(self, amount):
+        self.amount += amount
+        return "You've deposited ${} in the {} budget.".format(amount, self.category)
+
+    def withdraw(self, amount):
+        if self.amount <= amount:
+             return "You dont have enough funds in your {} budget to withdraw ${}!".format(self.category, amount)
+        else:
+            self.amount -= amount
+            return "You withdrew ${} from {} budget.".format(amount, self.category)
+            pass
+
+    def budget_balance(self):
+        return "Your current balance is: ${}" .format(self.amount)
+
+    def check_balance(self, amount):
+        #return self.amount
+        return "Your current {} balance is: ${}" .format(self.category, self.amount)
+        pass
+
+    def transfer(self, category):
+        if self.category >= self.category:
+            return "No need to transfer your balance"
+        else:
+            self.category += self.category
+            return "We have transferred funds from {} budget to {}.".format(self.category, self.category)
+            pass
+
+food_category = Budget("Food", 1000)
+clothing_category = Budget("Clothing", 1000)
+car_category = Budget("Car", 1000)
+
+# Adding money to balances
+print(food_category.deposit(1000))
+print(clothing_category.deposit(1000))
+
+# Checking balances
+print(food_category.check_balance(food_category))
+print(clothing_category.check_balance(clothing_category))
+print(car_category.check_balance(car_category))
+
+# making withdrawals
+print(food_category.withdraw(1500))
+print(clothing_category.withdraw(250))
+print(car_category.withdraw(1100))
+
+
+
